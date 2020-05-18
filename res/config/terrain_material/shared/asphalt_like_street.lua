6eb7dd34288ab705e2630daf04fcbddd0334ef92
lolluslollus <lolluslollus@outlook.com>
2020-05-19 01:22:49 +0200

    dirtier asphalt like street

--- res/config/terrain_material/shared/asphalt_like_street.lua
+++ res/config/terrain_material/shared/asphalt_like_street.lua
@@ -7,7 +7,7 @@ function data()
         overlayTexture = 'terrain/overlay_0.dds', -- dirt
         detailSize = 1 / 24, --.025, --1 / 15, -- the smaller the detail size, the larger the tiles will look. 1 / 15 is too small, 1/32 is too large
         overlaySize = 0.08, --0.05, -- the larger, the thicker the dirt
-        overlayStrength = 0.125, -- the larger, the stronger the overlay
+        overlayStrength = 0.2, -- 0.125, -- the larger, the stronger the overlay
         --hOffset = 0.3,
         order = -62,
         priority = 1000000
